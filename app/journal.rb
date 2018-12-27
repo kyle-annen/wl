ebb9c4e796fc7850d859c5ebe80a1db6316b3bdc
Kyle Annen <c_kannen@groupon.com>
2018-12-26 21:56:14 -0800

    add journal class

--- app/journal.rb
+++ app/journal.rb
@@ -1,13 +1,31 @@
 require "date"
 
 class Journal
-  attr_reader :directory
+  attr_reader :directory, :file_name, :file_path
 
-  def initialize(directory = "log")
-    @directory = directory
+  def initialize(date = Date.today, file = false)
+    @date = date
+    @directory = "/journals/"
+    @directory_path = File.dirname(__FILE__) + @directory
+    @file_name = file ? "file" : generate_file_name
+    @file_path = File.join(@directory_path, @file_name)
   end
 
-  def today
-    Date.today
+  def generate_file_name
+    @date.to_s + ".yml"
+  end
+
+  def add_entry(entry)
+    file = File.new(@file_path, "a")
+    file.puts(entry)
+    file.close
+  end
+
+  def last_entry
+    entries.last
+  end
+
+  def entries
+    File.read(@file_path).split("\n")
   end
 end
--- spec/app/journal_spec.rb
+++ spec/app/journal_spec.rb
@@ -2,17 +2,59 @@ require_relative "../../spec/spec_helper.rb"
 require_relative "../../app/journal.rb"
 
 describe Journal do
+  def test_journal_filename
+    "test-journal.yml"
+  end
+
+  def test_filepath
+    "./journals/#{test_journal_filename}"
+  end
+
+  def test_journal
+    Journal.new(file = "test-journal")
+  end
+
+  after(:each) do
+    File.delete(test_journal.file_path)
+  end
+
   describe "#initialize" do
     it "initialized with the default directory" do
-      journal = Journal.new
-      journal.directory.should == "log"
+      journal = test_journal
+      journal.add_entry("test")
+      journal.directory.should == "/journals/"
     end
   end
 
-  describe "today" do
-    it "returns the date of today" do
-      journal = Journal.new
-      journal.today.should == Date.today
+  describe "#file_name" do
+    it "returns the filename" do
+      journal = test_journal
+      journal.add_entry("test entry 1")
+
+      journal.file_name.should == test_journal_filename
+    end
+  end
+
+  describe "#add_entry" do
+    it "adds the entry to the last line of the log" do
+      journal = test_journal
+      test_entry = "this is only a test"
+
+      journal.add_entry(test_entry)
+      contents = File.read(journal.file_path).split("\n")
+
+      contents.last.should include(test_entry)
+    end
+  end
+
+  describe "#last_entry" do
+    it "returns the last entry of the day" do
+      journal = test_journal
+      test_entry = "modulating the bit manipulators"
+
+      journal.add_entry(test_entry)
+
+      journal.last_entry.should include(test_entry)
     end
   end
 end
